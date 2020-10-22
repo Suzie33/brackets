82acfedc907e8a138e77206031734535ab581044
Valentina Ponamareva <valiazin@gmail.com>
2020-10-22 14:41:44 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsList = bracketsConfig.flat();
+  let stack = [];
+
+  for (let char of str) {
+    let bracketInd = bracketsList.indexOf(char);
+
+    if (bracketsList[bracketInd] === bracketsList[bracketInd+1]) {
+      if (stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (bracketInd % 2 === 0) {
+      let closingBracket = bracketsList[bracketInd+1];
+      stack.push(closingBracket);
+    } else {
+      let lastStackChar = stack[stack.length-1];
+      if (char === lastStackChar) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  };
 }
